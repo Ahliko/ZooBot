5db73486903453b724d83e62de04ba6bfa8bbd13
mleo <leo.mace@ynov.com>
2022-10-21 19:29:11 +0200

    Other methode to do

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# ZooBot
+# **ZooBot**
 Bot for the Ynov's Zoo
+
+# Fonctionnalités
+
+## **Don de rôles via une réaction**
+
+<br>
+
+-> Donne un rôle correspondant à la réaction
+
+-> Créer automatiquement un salon vocal et textuel pour les personnes ayant le rôle
--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import discord as di
+from discord.ext import commands
 
 intents = di.Intents.default()
+
+
+
+#zooBot = commands.Bot(command_prefix="!", description="Ouh ouh ah ah !")
+
+#@zooBot.event
+#async def on_ready():
+#    print("I'm ready to be used !")
+
+#@zooBot.commands
+#async def hello(context,message):
+#    await context.send('Hello {0.author.mention}'.format(message))
+
+#zooBot.run("MTAzMjk4Nzc2ODk5MDc0NDU3Ng.GZ8jJ0.uClXTgvZRwlAZQewRjM5Veq4LNR-rnCZvevDk8")
 
 
 class MyClient(di.Client(intents=intents)):
