53d87f1d795ecd9df894fb88b16acc11c4ef6409
ahliko <killian.guillemot@ynov.com>
2022-10-21 14:44:10 +0200

    Add files

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord as di
+
+intents = di.Intents.default()
+
+
+class MyClient(di.Client(intents=intents)):
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
+
+    async def on_message(self, message):
+        # we do not want the bot to reply to itself
+        if message.author.id == self.user.id:
+            return
+
+        if message.content.startswith('!hello'):
+            await message.channel.send('Hello {0.author.mention}'.format(message))
+
+
+client = MyClient()
+client.run("MTAzMjk4Nzc2ODk5MDc0NDU3Ng.GZ8jJ0.uClXTgvZRwlAZQewRjM5Veq4LNR-rnCZvevDk8")
